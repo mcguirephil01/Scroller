8ba9b92aebf700e513016b79a8e152e57262e957
mcguirephil01 <mcguirephil01@gmail.com>
2014-10-23 15:24:22 -0400

    Initial Commit
    Initial Commit

---
+++ ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Scroller
+//
+//  Created by McGuire, Philip on 2/26/14.
+//  Copyright (c) 2014 McGuire, Philip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Scroller
+//
+//  Created by McGuire, Philip on 2/26/14.
+//  Copyright (c) 2014 McGuire, Philip. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
